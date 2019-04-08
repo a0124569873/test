a88965254bf8d326e4b8af08822aaa556b8b2edb
zxn <zxn@qq.com>
2019-04-08 15:53:06 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+body{
+  width: 100%;
+  height: 100%;
+  margin: 0px ! important;
+}
+html{
+  height: 100%;
+}
+#app{
+  height: 100%;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="home">
+    <el-container>
+      <el-header>
+        <el-menu :default-active="activeIndex" class mode="horizontal" @select="handleSelect">
+          <el-menu-item index="1">处理中心1</el-menu-item>
+          <el-menu-item index="2">处理中心2</el-menu-item>
+          <el-menu-item index="3">处理中心3</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <div>
+            <el-button v-for="item in tunnels">{{item}}</el-button>
+          </div>
+        </el-aside>
+        <el-main>
+          <router-view>
+
+          </router-view>
+        </el-main>
+      </el-container>
+      <el-footer>sdfsdf</el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {},
+  data() {
+    return {
+      activeIndex: 1,
+      tunnels: ["西篁隧道", "后塘沟隧道", "后塘沟隧道", "后塘沟隧道", "后塘沟隧道", "后塘沟隧道", "后塘沟隧道"]
+    };
+  },
+  methods: {
+    handleSelect() {
+      console.log();
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+
+
+.home{
+  height: 99%;
+}
+
+.el-container{
+  height: 100%
+}
+
+.el-header {
+
+}
+.el-footer {
+  // position: absolute;
+  // bottom: 0px;
+  text-align: center;
+  height: 20px ! important;
+
+}
+
+.el-aside {
+  // background-color: red;
+  padding: 50px 0px;
+  text-align: center;
+  height: 100%;
+  .el-button{
+    margin: 10px;
+    width: 150px;
+  }
+}
+
+.el-main {
+  // background-color: antiquewhite;
+  height: 100%;
+  text-align: center;
+}
+</style>
+
